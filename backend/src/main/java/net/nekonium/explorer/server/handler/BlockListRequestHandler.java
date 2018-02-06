a30f8b5611b68c41649c3a81a4a9dbe04557f7d4
Cupmouse <cupnmouse@gmail.com>
2018-02-06 09:36:00 +0900

    Updated paging on BlockListRequestHandler, improved performance

--- backend/src/main/java/net/nekonium/explorer/server/handler/BlockListRequestHandler.java
+++ backend/src/main/java/net/nekonium/explorer/server/handler/BlockListRequestHandler.java
@@ -50,7 +50,8 @@ public class BlockListRequestHandler implements RequestHandler<BlockListRequestH
 
             // Get last page number from database
 
-            final int lastPageNum = getLastPageNum(connection);      // Last page number
+            final long approxRowCount = getApproximateRowCount(connection);      // Last page number
+            final int lastPageNum = (int) ((approxRowCount - 1) / ELEMENT_IN_PAGE + 1);
             final int targetPageNum;
 
             if (parameters instanceof BlockListRequest.Page) {
@@ -61,7 +62,7 @@ public class BlockListRequestHandler implements RequestHandler<BlockListRequestH
                 throw new InvalidRequestException("Invalid parameter type");
             }
 
-            final JSONArray jsonArrayBlocks = querySearch(connection, targetPageNum);
+            final JSONArray jsonArrayBlocks = querySearch(connection, targetPageNum, approxRowCount);
 
             jsonArray.put(jsonArrayBlocks);
             jsonArray.put(lastPageNum);
@@ -78,46 +79,46 @@ public class BlockListRequestHandler implements RequestHandler<BlockListRequestH
         }
     }
 
-    private int getLastPageNum(Connection connection) throws SQLException {
-        int lastPageNum;
+    private long getApproximateRowCount(Connection connection) throws SQLException {
         final Statement statement = connection.createStatement();
 
-        final ResultSet resultSet = statement.executeQuery("SELECT COUNT(*) FROM blocks");
+        // internal_id is AUTO_INCLEMENT so always number < internal_id, row count <= internal_id
+        final ResultSet resultSet = statement.executeQuery("SELECT internal_id from blocks ORDER BY internal_id DESC LIMIT 1");
         resultSet.next();
         final long count = resultSet.getLong(1);
 
         resultSet.close();
         statement.close();
 
-        lastPageNum = (int) ((count - 1) / ELEMENT_IN_PAGE + 1);
-        return lastPageNum;
+        return count;
     }
 
-    private JSONArray querySearch(Connection connection, int targetPageNum) throws SQLException {
+    private JSONArray querySearch(Connection connection, int targetPageNum, long approxRowCount) throws SQLException {
         final JSONArray jsonArray = new JSONArray();
 
         final PreparedStatement prpstmt = connection.prepareStatement(
-                "SELECT blocks.number, NEKH(blocks.hash), NEKH(addresses.address), " +
+                "SELECT blocks.internal_id, blocks.number, NEKH(blocks.hash), NEKH(addresses.address), " +
                         "(SELECT COUNT(*) FROM transactions WHERE transactions.block_id = blocks.internal_id), " +
                         "(SELECT COUNT(*) FROM uncle_blocks WHERE uncle_blocks.block_id = blocks.internal_id), " +
                         "blocks.forked, blocks.internal_id FROM blocks " +
                         "LEFT JOIN addresses ON addresses.internal_id = blocks.miner_id " +
-                        "WHERE blocks.internal_id " +
-                        "ORDER BY number DESC LIMIT ? OFFSET ?");
-        prpstmt.setLong(1, ELEMENT_IN_PAGE);
-        prpstmt.setLong(2, ELEMENT_IN_PAGE * (targetPageNum - 1));
+                        "WHERE blocks.internal_id BETWEEN ? AND ? " +
+                        "ORDER BY number DESC");
+        prpstmt.setLong(1, approxRowCount - ELEMENT_IN_PAGE * targetPageNum + 1);   // Note: This is smaller than 👇
+        prpstmt.setLong(2, approxRowCount - ELEMENT_IN_PAGE * (targetPageNum - 1)); // This is LARGER than 👆
 
         final ResultSet resultSet = prpstmt.executeQuery();
 
         while (resultSet.next()) {
             final JSONArray jsonArrayBlock = new JSONArray();
 
-            jsonArrayBlock.put(resultSet.getString(1));                // Block Number
-            jsonArrayBlock.put(resultSet.getString(2));                // Hash
-            jsonArrayBlock.put(resultSet.getString(3));                // Miner address
-            jsonArrayBlock.put(resultSet.getInt(4));                   // Transaction count
-            jsonArrayBlock.put(resultSet.getInt(5));                   // Uncle block count
-            jsonArrayBlock.put(resultSet.getBoolean(6));               // Is forked
+            jsonArrayBlock.put(resultSet.getString(1));                // Internal id
+            jsonArrayBlock.put(resultSet.getString(2));                // Block number
+            jsonArrayBlock.put(resultSet.getString(3));                // Hash
+            jsonArrayBlock.put(resultSet.getString(4));                // Miner address
+            jsonArrayBlock.put(resultSet.getInt(5));                   // Transaction count
+            jsonArrayBlock.put(resultSet.getInt(6));                   // Uncle block count
+            jsonArrayBlock.put(resultSet.getBoolean(7));               // Is forked
 
             jsonArray.put(jsonArrayBlock);
         }
